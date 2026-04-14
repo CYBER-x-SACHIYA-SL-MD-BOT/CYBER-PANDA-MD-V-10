92a6df2d663b1665364ca0dba20421ed32d5752f
Sachith Chandra <133668461+CYBER-x-SACHIYA-SL-MD-BOT@users.noreply.github.com>
2026-04-14 23:32:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎CYBER-PANDA-MD',
   botName: "CYBER-PANDA-MD",
   botOwner: 'Sachith', // Your name
-  ownerNumber: '94717233347', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+94702782946', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
