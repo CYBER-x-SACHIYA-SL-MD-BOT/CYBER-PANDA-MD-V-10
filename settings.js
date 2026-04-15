1db87653d98971abdb5ea8390e2eaa66d051c3be
Sachith Chandra <133668461+CYBER-x-SACHIYA-SL-MD-BOT@users.noreply.github.com>
2026-04-15 13:48:56 +0530

    🐼 𝘾𝙔𝘽𝙀𝙍 𝙋𝘼𝙉𝘿𝘼 𝙈𝘿 𝙑.10 🐼

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'CYBER-PANDA-MD',
-  author: '‎CYBER-PANDA-MD',
-  botName: "CYBER-PANDA-MD",
+  packname: 'CYBER-PANDA-MD-V.10',
+  author: 'CYBER-PANDA-MD-V.10',
+  botName: "CYBER-PANDA-MD-V.10",
   botOwner: 'Sachith', // Your name
   ownerNumber: '+94702782946', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: " Overthink Kills Your Happiness 🤍😊",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD-V-10/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
